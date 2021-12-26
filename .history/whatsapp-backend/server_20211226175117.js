5b21a1d927fbc79e32331ed2e60cf605e8fae7ea
nadavmza <nadavmza89@gmail.com>
2021-12-26 17:56:40 +0200

    connect to DB

---
+++ .history/whatsapp-backend/server_20211226175117.js
@@ -0,0 +1,21 @@
+
+import express from 'express';
+import mongoose from 'mongoose'
+// importing
+
+
+//app config
+const app =express();
+const port=process.env.PORT || 9000;
+//middleware
+
+//DB config
+
+
+//??
+
+//api routes
+app.get('/',(req,res)=>res.status(200).send('hello world'));
+
+//listen
+app.listen(port,()=> console.log(`Listening to localhost:${port}`));
