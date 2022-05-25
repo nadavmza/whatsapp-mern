7d807c2d07a4eba8f6faa4f4d3c7f877fc732bfe
nadavmza <nadavmza89@gmail.com>
2022-05-25 13:20:59 +0300

    version 1.0.2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
-import Messages from "/dbMessages.js";
+import Messages from "./dbMessages.js";
 import Pusher from "pusher";
 //  import cors from 'cors';
 // importing
